6c0504aaefcdfe2295c07fe51d08fd658d05cf22
ronynx <skc.academia@gmail.com>
2026-02-27 21:52:53 +0545

    Create index.php

---
+++ index.php
@@ -0,0 +1,22 @@
+<?php
+include("includes/header.php");
+?>
+
+<div id="content">
+    
+    <h1>Welcome to eStore</h1>
+    <p>Your online shopping destination for quality products.</p>
+
+    <div class="slider">
+        <div class="slides">
+            <img src="assets/images/slider1.jpg" alt="Slide 1">
+            <img src="assets/images/slider2.jpg" alt="Slide 2">
+            <img src="assets/images/slider3.jpg" alt="Slide 3">
+        </div>
+    </div>
+
+</div>
+
+<?php
+include("includes/footer.php");  
+?>
