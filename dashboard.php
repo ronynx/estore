592699399370c6b4b4b58024615cdf888961df1c
ronynx <skc.academia@gmail.com>
2026-03-03 12:31:18 +0545

    Remove dashboard actions links
    Removed dashboard actions links for homepage and logout.

--- dashboard.php
+++ dashboard.php
@@ -7,11 +7,6 @@ require_once "includes/auth.php";
     <div id="dashboard">
         <h1>Welcome, <?php echo htmlspecialchars($_SESSION['first_name']); ?>!</h1>
         <p>You have successfully logged in to your eStore dashboard.</p>
-
-        <div class="dashboard-actions">
-            <a href="index.php">Go to Homepage</a> |
-            <a href="logout.php">Logout</a>
-        </div>
     </div>
 </div>
 
